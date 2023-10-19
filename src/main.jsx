72f7a1e3a6aed3a53e8be7a95eff671f6f64df46
DAVIDSON IDADAKPOYE <davebelovedcity@gmail.com>
2023-10-19 17:43:22 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { StateContextProvider } from "./context/contextProvider.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <StateContextProvider>
         <App />
       </StateContextProvider>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
